daa535ef43a971c1565fecc3c1e3eb970a093a94
EvonDu <298550274@qq.com>
2019-04-30 14:55:28 +0800

    调整创建支付分订单例子的开始时间,防止由于调用时间差等造成接口调用失败

--- demo/payscore/create.php
+++ demo/payscore/create.php
@@ -16,7 +16,7 @@ date_default_timezone_set('PRC');
 $result = $client->payscore->create([
     "out_order_no" => uniqid(),
     "service_id"  => "00004000000000523335451575645446",
-    "service_start_time" => date("YmdHis"),
+    "service_start_time" => date("YmdHis",strtotime("+30 seconds")),
     "service_end_time" => date("YmdHis",strtotime("+1 day")),
     "service_start_location" => "利保机器人重力柜",
     "service_end_location" => "利保机器人重力柜",
